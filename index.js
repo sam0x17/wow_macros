775e3728464a8c039181a038820e2519989bb3c4
Sam Johnson <sam@durosoft.com>
2019-09-14 00:49:06 -0400

    hello world test

---
+++ index.js
@@ -0,0 +1,12 @@
+// Type "Hello World" then press enter.
+var robot = require("robotjs");
+
+// Press enter.
+robot.keyTap("enter");
+
+// Type "Hello World".
+robot.typeString("/s Hello World");
+
+// Press enter.
+robot.keyTap("enter");
+
